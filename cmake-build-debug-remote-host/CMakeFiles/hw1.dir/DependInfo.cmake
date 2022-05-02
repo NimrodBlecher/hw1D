e336fcd01299ba6b32567b84e5206a84ac1831a6
yuvalrosman <yuvalrosman@campus.technion.ac.il>
2022-05-02 13:08:12 +0300

    new start

---
+++ cmake-build-debug-remote-host/CMakeFiles/hw1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yuvalrosman/dast_new/Comapny.cpp" "/home/yuvalrosman/dast_new/cmake-build-debug-remote-host/CMakeFiles/hw1.dir/Comapny.cpp.o"
+  "/home/yuvalrosman/dast_new/Employee.cpp" "/home/yuvalrosman/dast_new/cmake-build-debug-remote-host/CMakeFiles/hw1.dir/Employee.cpp.o"
+  "/home/yuvalrosman/dast_new/Market.cpp" "/home/yuvalrosman/dast_new/cmake-build-debug-remote-host/CMakeFiles/hw1.dir/Market.cpp.o"
+  "/home/yuvalrosman/dast_new/helpers.cpp" "/home/yuvalrosman/dast_new/cmake-build-debug-remote-host/CMakeFiles/hw1.dir/helpers.cpp.o"
+  "/home/yuvalrosman/dast_new/main.cpp" "/home/yuvalrosman/dast_new/cmake-build-debug-remote-host/CMakeFiles/hw1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
