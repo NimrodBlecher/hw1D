3705fb0fe5615868fa87af4e8f340e64603b26fc
NimrodBlecher <96698306+NimrodBlecher@users.noreply.github.com>
2022-05-02 12:02:09 +0300

    changes to market

--- main.cpp
+++ main.cpp
@@ -9,115 +9,118 @@ using namespace hw1;
 
 int main() {
 
+//
+//Market highTech;
+//
+//highTech.addCompany(1,100);
+//highTech.addCompany(2,1200);
+////highTech.addCompany(2,1500);
+//
+//highTech.addEmployee(2,1,5,500);
+//highTech.addEmployee(2,2,5,600);
+//highTech.addEmployee(2,3,5,500);
+//highTech.addEmployee(2,4,5,600);
+//highTech.addEmployee(2,5,5,500);
+//highTech.addEmployee(2,6,5,600);
+//
+//
+//
+//highTech.addEmployee(1,15,5,700);
+////highTech.addEmployee(1,1,5,700);
+////highTech.addEmployee(1,2,5,700);
+//highTech.addEmployee(1,16,5,700);
+//highTech.addEmployee(1,17,5,700);
+//
+//
+//
+//AVLnode<Company*,int>* companies = highTech.getCompaniesTree();
+//    cout << "Companies By Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//print2D(companies);
+//cout << "Employees By Id is : " << endl;
+//cout << endl << endl << endl << endl;
+//print2D(highTech.getEmployeesTreeById());
+//    cout << "Employees Route By Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//print2D(highTech.getEmployeesRouteTree());
+//    cout << "Employees By Salary Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//print2D(highTech.getEmployeesTreeBySalary());
+//    cout << "Employees In Company 1 is : " << endl;
+//    cout << endl << endl << endl << endl;
+//print2D(highTech.getCompany(1) -> getEmployeesTreeById());
+//    cout << "Employees In Company 2 is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getCompany(2) -> getEmployeesTreeById());
+//
+//int x,y;
+//highTech.getCompanyInfo(2,&x,&y);
+//cout<<"value is: " << x<< "    number of employees is: " << y;
+//
+//highTech.removeEmployee(3);
+//    cout << "Companies By Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(companies);
+//    cout << "Employees By Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getEmployeesTreeById());
+//    cout << "Employees Route By Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getEmployeesRouteTree());
+//    cout << "Employees By Salary Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getEmployeesTreeBySalary());
+//    cout << "Employees In Company 1 is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getCompany(1) -> getEmployeesTreeById());
+//    cout << "Employees In Company 2 is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getCompany(2) -> getEmployeesTreeById());
+//highTech.removeEmployee(2);
+//highTech.removeEmployee(4);
+//highTech.removeEmployee(1);
+//highTech.removeEmployee(5);
+//cout << "Companies By Id is : " << endl;
+//cout << endl << endl << endl << endl;
+//print2D(companies);
+//cout << "Employees By Id is : " << endl;
+//cout << endl << endl << endl << endl;
+//print2D(highTech.getEmployeesTreeById());
+//cout << "Employees Route By Id is : " << endl;
+//cout << endl << endl << endl << endl;
+//print2D(highTech.getEmployeesRouteTree());
+//cout << "Employees By Salary Id is : " << endl;
+//cout << endl << endl << endl << endl;
+//print2D(highTech.getEmployeesTreeBySalary());
+//cout << "Employees In Company 1 is : " << endl;
+//cout << endl << endl << endl << endl;
+//print2D(highTech.getCompany(1) -> getEmployeesTreeById());
+//cout << "Employees In Company 2 is : " << endl;
+//cout << endl << endl << endl << endl;
+//print2D(highTech.getCompany(2) -> getEmployeesTreeById());
+//highTech.removeEmployee(6);
+//print2D(highTech.getCompany(2) -> getEmployeesTreeById());
+//highTech.removeCompany(2);
+//    cout << "Companies By Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(companies);
+//    cout << "Employees By Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getEmployeesTreeById());
+//    cout << "Employees Route By Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getEmployeesRouteTree());
+//    cout << "Employees By Salary Id is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getEmployeesTreeBySalary());
+//    cout << "Employees In Company 1 is : " << endl;
+//    cout << endl << endl << endl << endl;
+//    print2D(highTech.getCompany(1) -> getEmployeesTreeById());
+//
 
-Market highTech;
-
-highTech.addCompany(1,100);
-highTech.addCompany(2,1200);
-//highTech.addCompany(2,1500);
-
-highTech.addEmployee(2,1,5,500);
-highTech.addEmployee(2,2,5,600);
-highTech.addEmployee(2,3,5,500);
-highTech.addEmployee(2,4,5,600);
-highTech.addEmployee(2,5,5,500);
-highTech.addEmployee(2,6,5,600);
-
-
-
-highTech.addEmployee(1,15,5,700);
-//highTech.addEmployee(1,1,5,700);
-//highTech.addEmployee(1,2,5,700);
-highTech.addEmployee(1,16,5,700);
-highTech.addEmployee(1,17,5,700);
-
-
-
-AVLnode<Company*,int>* companies = highTech.getCompaniesTree();
-    cout << "Companies By Id is : " << endl;
-    cout << endl << endl << endl << endl;
-print2D(companies);
-cout << "Employees By Id is : " << endl;
-cout << endl << endl << endl << endl;
-print2D(highTech.getEmployeesTreeById());
-    cout << "Employees Route By Id is : " << endl;
-    cout << endl << endl << endl << endl;
-print2D(highTech.getEmployeesRouteTree());
-    cout << "Employees By Salary Id is : " << endl;
-    cout << endl << endl << endl << endl;
-print2D(highTech.getEmployeesTreeBySalary());
-    cout << "Employees In Company 1 is : " << endl;
-    cout << endl << endl << endl << endl;
-print2D(highTech.getCompany(1) -> getEmployeesTreeById());
-    cout << "Employees In Company 2 is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getCompany(2) -> getEmployeesTreeById());
-
-int x,y;
-highTech.getCompanyInfo(2,&x,&y);
-cout<<"value is: " << x<< "    number of employees is: " << y;
-
-highTech.removeEmployee(3);
-    cout << "Companies By Id is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(companies);
-    cout << "Employees By Id is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getEmployeesTreeById());
-    cout << "Employees Route By Id is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getEmployeesRouteTree());
-    cout << "Employees By Salary Id is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getEmployeesTreeBySalary());
-    cout << "Employees In Company 1 is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getCompany(1) -> getEmployeesTreeById());
-    cout << "Employees In Company 2 is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getCompany(2) -> getEmployeesTreeById());
-highTech.removeEmployee(2);
-highTech.removeEmployee(4);
-highTech.removeEmployee(1);
-highTech.removeEmployee(5);
-cout << "Companies By Id is : " << endl;
-cout << endl << endl << endl << endl;
-print2D(companies);
-cout << "Employees By Id is : " << endl;
-cout << endl << endl << endl << endl;
-print2D(highTech.getEmployeesTreeById());
-cout << "Employees Route By Id is : " << endl;
-cout << endl << endl << endl << endl;
-print2D(highTech.getEmployeesRouteTree());
-cout << "Employees By Salary Id is : " << endl;
-cout << endl << endl << endl << endl;
-print2D(highTech.getEmployeesTreeBySalary());
-cout << "Employees In Company 1 is : " << endl;
-cout << endl << endl << endl << endl;
-print2D(highTech.getCompany(1) -> getEmployeesTreeById());
-cout << "Employees In Company 2 is : " << endl;
-cout << endl << endl << endl << endl;
-print2D(highTech.getCompany(2) -> getEmployeesTreeById());
-highTech.removeEmployee(6);
-print2D(highTech.getCompany(2) -> getEmployeesTreeById());
-highTech.removeCompany(2);
-    cout << "Companies By Id is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(companies);
-    cout << "Employees By Id is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getEmployeesTreeById());
-    cout << "Employees Route By Id is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getEmployeesRouteTree());
-    cout << "Employees By Salary Id is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getEmployeesTreeBySalary());
-    cout << "Employees In Company 1 is : " << endl;
-    cout << endl << endl << endl << endl;
-    print2D(highTech.getCompany(1) -> getEmployeesTreeById());
-
-
-
+int*  x = new int(6);
+int*  y = new int(5);
+int*  z = new int(4);
+AVLnode<int*,int>* tree = new
  return 0;
 }
