853290cf27a1bbe041c49d95860382191a8fd4b2
NimrodBlecher <96698306+NimrodBlecher@users.noreply.github.com>
2022-05-03 12:24:01 +0300

    changes to bvbbbb

--- CHANGES.cpp
+++ CHANGES.cpp
@@ -1,54 +1,239 @@
-#include "Market.h"
+#include "library1.h"
+#include "PlayersManager.h"
 
-
-
-Market :: ~Market() {
-    deleteTreeWithData(companies_tree);
-    deleteTree(market_employees_salary_tree);
-    deleteTree(companies_with_employees_tree);
-    deleteTree(market_employee_id_tree);
-};
-
-
-
-
-Company :: ~Company() {
-    deleteTree(employees_tree_by_salary);
-    deleteTreeWithData(employees_tree_by_id);
+void* Init()
+{
+    PlayersManager* DS;
+    try
+    {
+        DS = new PlayersManager();
+    }
+    catch (std::bad_alloc& bad_alloc)
+    {
+        return nullptr;
+    }
+    return DS;
 }
 
+StatusType AddGroup(void *DS, int GroupID)
+{
+    if (DS == nullptr)
+    {
+        return INVALID_INPUT;
+    }
+    try
+    {
+        (static_cast<PlayersManager*>(DS))-> addGroup(GroupID);
+    }
+    catch (InvalidInput& invalid_input)
+    {
+        return INVALID_INPUT;
+    }
+    catch (std::bad_alloc& bad_alloc)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (GroupIDIsAlreadyTaken& group_id_already_taken)
+    {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
 
-template<class Data, class Key>
-void deleteTreeWithData(AVLnode<Data,Key>* root) {
-    if(root == nullptr)
+StatusType AddPlayer(void *DS, int PlayerID, int GroupID, int Level)
+{
+    if (DS == nullptr)
+    {
+        return INVALID_INPUT;
+    }
+    try
+    {
+        (static_cast<PlayersManager*>(DS))->addPlayer(PlayerID, GroupID, Level);
+    }
+    catch (InvalidInput& invalid_input)
+    {
+        return INVALID_INPUT;
+    }
+    catch (std::bad_alloc& bad_alloc)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (GroupDoesNotExist& group_does_not_exists)
+    {
+        return FAILURE;
+    }
+    catch (PlayerIDIsAlreadyTaken& player_id_already_taken)
+    {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType RemovePlayer(void *DS, int PlayerID)
+{
+    if (DS == nullptr)
+    {
+        return INVALID_INPUT;
+    }
+    try
+    {
+        (static_cast<PlayersManager*>(DS)->removePlayer(PlayerID));
+    }
+    catch (InvalidInput& invalid_input)
+    {
+        return INVALID_INPUT;
+    }
+    catch (std::bad_alloc& bad_alloc)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (PlayerDoesNotExist& player_does_not_exists)
+    {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType ReplaceGroup(void *DS, int GroupID, int ReplacementID)
+{
+    if (DS == nullptr)
+    {
+        return INVALID_INPUT;
+    }
+    try
+    {
+        (static_cast<PlayersManager*>(DS)->replaceGroup(GroupID, ReplacementID));
+    }
+    catch (InvalidInput& invalid_input)
+    {
+        return INVALID_INPUT;
+    }
+    catch (std::bad_alloc& bad_alloc)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (GroupDoesNotExist& player_does_not_exists)
+    {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType IncreaseLevel(void *DS, int PlayerID, int LevelIncrease)
+{
+    if (DS == nullptr)
+    {
+        return INVALID_INPUT;
+    }
+    try
+    {
+        (static_cast<PlayersManager*>(DS)->increasePlayerLevel(PlayerID, LevelIncrease));
+    }
+    catch (InvalidInput& invalid_input)
+    {
+        return INVALID_INPUT;
+    }
+    catch (std::bad_alloc& bad_alloc)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (PlayerDoesNotExist& player_does_not_exists)
+    {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType GetHighestLevel(void *DS, int GroupID, int *PlayerID)
+{
+    if (DS == nullptr)
+    {
+        return INVALID_INPUT;
+    }
+    try
+    {
+        (static_cast<PlayersManager*>(DS)->getLeader(GroupID, PlayerID));
+    }
+    catch (InvalidInput& invalid_input)
+    {
+        return INVALID_INPUT;
+    }
+    catch (std::bad_alloc& bad_alloc)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (GroupDoesNotExist& group_does_not_exists)
+    {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType GetAllPlayersByLevel(void *DS, int GroupID, int **Players, int *numOfPlayers)
+{
+    if (DS == nullptr)
+    {
+        return INVALID_INPUT;
+    }
+    try
+    {
+        (static_cast<PlayersManager*>(DS)->
+                getAllPlayersByLevel(GroupID, Players, numOfPlayers));
+    }
+    catch (InvalidInput& invalid_input)
+    {
+        return INVALID_INPUT;
+    }
+    catch (std::bad_alloc& bad_alloc)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (AllocationError& allocation_error)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (GroupDoesNotExist& group_does_not_exists)
+    {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+StatusType GetGroupsHighestLevel(void *DS, int numOfGroups, int **Players)
+{
+    if (DS == nullptr)
+    {
+        return INVALID_INPUT;
+    }
+    try
+    {
+        (static_cast<PlayersManager*>(DS)->getGroupsLeaders(numOfGroups, Players));
+    }
+    catch (InvalidInput& invalid_input)
+    {
+        return INVALID_INPUT;
+    }
+    catch (std::bad_alloc& bad_alloc)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (AllocationError& allocation_error)
+    {
+        return ALLOCATION_ERROR;
+    }
+    catch (TooFewFullGroups& too_few_full_groups)
+    {
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+void Quit(void **DS)
+{
+    if(DS == nullptr)
     {
         return;
     }
-    if (root->getRight() == nullptr && root->getLeft() == nullptr) {
-        delete (root -> getData());
-        delete root;
-        return;
-    }
-    if (root->getLeft() != nullptr) {
-        deleteTree(root->getLeft());
-        root -> setLeft(nullptr);
-    }
-    if (root->getRight() != nullptr) {
-        deleteTree(root->getRight());
-        root ->setRight(nullptr);
-    }
-    delete (root -> getData());
-    deleteTree(root);
+    delete static_cast<PlayersManager*>(*DS);
+    *DS = nullptr;
 }
-
-
-
-
-
-
-
-
-
-
-}
-
