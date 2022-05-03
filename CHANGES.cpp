8b8b06237902f4fa36690ab513a8e14f28101fa5
NimrodBlecher <96698306+NimrodBlecher@users.noreply.github.com>
2022-05-03 11:53:14 +0300

    changes to bvbbbb

--- CHANGES.cpp
+++ CHANGES.cpp
@@ -1,46 +1,54 @@
-//#include "Market.h"
-//
-//// ewfwerfwerfwef
-//void inOrderToEmployeesArray(int** employees_array,int* start, AVLnode<Company*,int>* root,int* times_left);
-//
-//void Market ::getHighestEarnerInEachCompany(int number_of_companies_to_find, int **employees_by_id) {
-//    if (number_of_companies_to_find == 0 || employees_by_id == nullptr)
-//    {
-//        throw InvalidInput();
-//    }
-//    if (num_of_company_with_employees < number_of_companies_to_find)
-//    {
-//        throw NotEnoughCompaniesWithEmployees();
-//    }
-//    int** array_to_return = new int*[number_of_companies_to_find];
-//    int x = 0 , y = number_of_companies_to_find;
-//    AVLnode<Company*,int>* root = companies_with_employees_tree;
-//    inOrderToEmployeesArray(array_to_return,&x,root,&y);
-//    employees_by_id = array_to_return;
-//}
-//
-//
-//
-//void inOrderToEmployeesArray(int** employees_array,int* start, AVLnode<Company*,int>* root,int* times_left)
-//{
-//    if (root == nullptr || *times_left == 0) {
-//        return;
-//    }
-//    if (root->getLeft() != nullptr && *times_left > 0) {
-//        inOrderToEmployeesArray(employees_array,start,root->getLeft(),times_left);
-//    }
-////        cout << root->getKey1() << ", " << endl;
-////        cout << "start is : " << *start << endl;
-//    root -> getData() ->getHighestEarner(employees_array[*start]);
-//    (*start)++;
-//    (*times_left)--;
-//    if (root->getRight() != nullptr && *times_left > 0) {
-//        inOrderToEmployeesArray(employees_array, start, root->getRight(),times_left);
-//    }
-//}
-//
-//
-//
+#include "Market.h"
 
 
+
+Market :: ~Market() {
+    deleteTreeWithData(companies_tree);
+    deleteTree(market_employees_salary_tree);
+    deleteTree(companies_with_employees_tree);
+    deleteTree(market_employee_id_tree);
+};
+
+
+
+
+Company :: ~Company() {
+    deleteTree(employees_tree_by_salary);
+    deleteTreeWithData(employees_tree_by_id);
+}
+
+
+template<class Data, class Key>
+void deleteTreeWithData(AVLnode<Data,Key>* root) {
+    if(root == nullptr)
+    {
+        return;
+    }
+    if (root->getRight() == nullptr && root->getLeft() == nullptr) {
+        delete (root -> getData());
+        delete root;
+        return;
+    }
+    if (root->getLeft() != nullptr) {
+        deleteTree(root->getLeft());
+        root -> setLeft(nullptr);
+    }
+    if (root->getRight() != nullptr) {
+        deleteTree(root->getRight());
+        root ->setRight(nullptr);
+    }
+    delete (root -> getData());
+    deleteTree(root);
+}
+
+
+
+
+
+
+
+
+
+
+}
 
